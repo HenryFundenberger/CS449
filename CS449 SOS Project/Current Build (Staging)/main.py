2c6a98fb08bbd6211a4f2c526df581fbede5af88
unknown <henryfundenberger@gmail.com>
2022-09-13 12:33:41 -0500

    Added More GUI Practice and Ideas for developing

---
+++ CS449 SOS Project/Current Build (Staging)/main.py
@@ -0,0 +1,101 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("App")
+        self.geometry("600x300")
+        self.resizable(True, True)
+        self.create_widgets()
+        
+
+    def create_widgets(self):
+
+       #Create label in center of window
+        self.label = tk.Label(self, text="SOS", font=("Arial", 20))
+        self.label.pack(pady=10)
+
+        #Create frame for buttons
+        self.frame = tk.Frame(self)
+        self.frame.size = 3
+        self.frame.pack(pady=10)
+
+        #Create 8x8 grid of buttons in frame
+        for row in range(3):
+            for column in range(3):
+                #Remove Space between buttons
+                self.frame.columnconfigure(column, weight=1)
+                button = ttk.Button(self.frame, text=" ", width=3, command=self.clicked)
+                button.grid(row=row, column=column, padx=0, pady=0)
+
+
+        #Create controls frame under buttons
+        self.controls_frame = tk.Frame(self)
+        self.controls_frame.pack(pady=5)
+
+        #Create Player 1 and Player 2 labels
+        self.player1_label = tk.Label(self.controls_frame, text="Player 1", font=("Arial", 10))
+        self.player1_label.grid(row=0, column=0, padx=10, pady=10)
+
+        #Create 2 Radio Buttons for Player 1
+        self.player1_var = tk.StringVar()
+        self.player1_var.set("X")
+        self.player1_x = tk.Radiobutton(self.controls_frame, text="X", variable=self.player1_var, value="X")
+        self.player1_x.grid(row=1, column=0, padx=10, pady=10)
+        self.player1_o = tk.Radiobutton(self.controls_frame, text="O", variable=self.player1_var, value="O")
+        self.player1_o.grid(row=2, column=0, padx=10, pady=10)
+
+
+        self.player2_label = tk.Label(self.controls_frame, text="Player 2", font=("Arial", 10))
+        # Create 2 Radio Buttons for Player 2
+        self.player2_var = tk.StringVar()
+        self.player2_var.set("O")
+        self.player2_x = tk.Radiobutton(self.controls_frame, text="X", variable=self.player2_var, value="X")
+        self.player2_x.grid(row=1, column=1, padx=10, pady=10)
+        self.player2_o = tk.Radiobutton(self.controls_frame, text="O", variable=self.player2_var, value="O")
+        self.player2_o.grid(row=2, column=1, padx=10, pady=10)
+
+
+        self.player2_label.grid(row=0, column=1, padx=10, pady=10)
+
+
+
+    
+
+    def clicked(self):
+        #Get Clicked Button
+        button = self.focus_get()
+        #Current Player
+        player = self.player1_var.get()
+
+        #Change Button Text to X or O
+        if button["text"] == " ":
+            if player == "X":
+                button["text"] = "X"
+            else:
+                button["text"] = "O"
+        else:
+            messagebox.showerror("Error", "Button already clicked")
+        
+        
+
+        
+    
+
+        
+
+
+    def button_clicked(self):
+        # Change Button text to X or O
+        button = self.focus_get()
+        
+
+        
+        
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
