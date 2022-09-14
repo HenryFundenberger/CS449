624561e760868e6637d34e5658314fb1233aacc3
unknown <henryfundenberger@gmail.com>
2022-09-14 14:20:47 -0500

    Added Tests

---
+++ CS449 SOS Project/Sprint 0/test.py
@@ -0,0 +1,21 @@
+import mathlib
+ 
+def test_calc_addition():
+    '''Verify the output of `calc_addition` function'''
+    output = 2+4
+    assert output == 6
+ 
+def test_calc_substraction():
+    '''Verify the output of `calc_substraction` function'''
+    output = 2-4
+    assert output == -2
+     
+def test_calc_multiply():
+    '''Verify the output of `calc_multiply` function'''
+    output = 2*4
+    assert output == 8
+
+def test_calc_divide():
+    '''Verify the output of `calc_divide` function'''
+    output = 2/4
+    assert output == 0.5
